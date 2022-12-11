16bd45c798d9670a8a16d34ea754250443add3cd
Sun <sonya8412@gmail.com>
2022-12-11 21:35:16 +0900

    Update ball.py
    finished ball game

--- basic/ball.py
+++ basic/ball.py
@@ -1,22 +1,27 @@
 import pygame
 # 게임 스크린 크기
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 700
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 # 색상 전역변수
-BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-RED = (200, 0, 0)
-GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 # pygame 초기화
 pygame.init()
 # 윈도우 제목
-pygame.display.set_caption("name")
+pygame.display.set_caption("Ball")
 # 스크린 정의
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 # 게임 화면 업데이트 속도
 clock = pygame.time.Clock()
+
+# 공 초기 위치, 크기, 속도
+ball_x = int(SCREEN_WIDTH / 2)
+ball_y = int(SCREEN_HEIGHT / 2)
+ball_dx = 4
+ball_dy = 4
+ball_size = 40
+
 # 게임 진행 여부를 판단하는 변수 : 게임 종료 전까지 반복
 done = False 
 # 게임 반복 구간
@@ -27,13 +32,22 @@ while not done:
             done = True
 
     # 게임 로직 구간
-
+    # 속도에 따라 원형 위치 변경
+    ball_x += ball_dx
+    ball_y += ball_dy
+    # 공이 스크린을 벗어날 경우
+    if(ball_x + ball_size) > SCREEN_WIDTH or (ball_x - ball_size) < 0:
+        ball_dx = ball_dx * -1
+    if(ball_y + ball_size) > SCREEN_HEIGHT or (ball_y - ball_size) < 0:
+        ball_dy = ball_dy * -1
+    
     # 화면 삭제 구간
 
     # 스크린 채우기
     screen.fill(WHITE)
 
     # 화면 그리기 구간
+    pygame.draw.circle(screen, BLUE, [ball_x, ball_y], ball_size, 0)
 
     # 화면 업데이트
     pygame.display.flip()
