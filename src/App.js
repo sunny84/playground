5262f6e87122fbb813d1786f1d9d94f1ae938b41
sunny84 <sonya8412@gmail.com>
2023-12-29 09:40:45 +0900

    react state

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 class App extends Component {
 
-  initialExpenses = [
-    { id: 1, charge: "렌트비", amount: 1600 },
-    { id: 2, charge: "교통비", amount: 400 },
-    { id: 3, charge: "식비", amount: 1200 },
-  ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      expenses: [
+        { id: 1, charge: "렌트비", amount: 1600 },
+        { id: 2, charge: "교통비", amount: 400 },
+        { id: 3, charge: "식비", amount: 1200 }
+      ]
+    }
+  }
 
   handleDelete = (id) => {
-    const newExpenses = this.initialExpenses.filter(expense => expense.id !== id)
+    const newExpenses = this.state.expenses.filter(expense => expense.id !== id)
     console.log(newExpenses);
+    this.setState({ expenses: newExpenses });
   }
 
   render() {
@@ -26,7 +32,7 @@ class App extends Component {
       <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
         {/* {Expense List} */}
         <ExpenseList 
-          initialExpenses={this.initialExpenses}
+          initialExpenses={this.state.expenses}
           handleDelete={this.handleDelete}
         />
       </div>
