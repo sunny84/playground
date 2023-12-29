f7ed1a5d72d46fbea117390191a1eb955b5f92da
sunny84 <sonya8412@gmail.com>
2023-12-29 09:33:46 +0900

    예산 계산기 만들기 앱1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  initialExpenses = [
+    { id: 1, charge: "렌트비", amount: 1600 },
+    { id: 2, charge: "교통비", amount: 400 },
+    { id: 3, charge: "식비", amount: 1200 },
+  ]
+
+  handleDelete = (id) => {
+    const newExpenses = this.initialExpenses.filter(expense => expense.id !== id)
+    console.log(newExpenses);
+  }
+
+  render() {
+    return (
+    <main className="main-container">
+      <h1>예산 계산기</h1>
+      <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
+        {/* {Expense List} */}
+        <ExpenseForm></ExpenseForm>
+      </div>
+      <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
+        {/* {Expense List} */}
+        <ExpenseList 
+          initialExpenses={this.initialExpenses}
+          handleDelete={this.handleDelete}
+        />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'end', marginTop: '1rem' }}>
+        <p style={{ fontSize: '2rem' }}>
+          총지출 : 
+          <span>원</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    </main>
+    )
+  }
 }
 
 export default App;
